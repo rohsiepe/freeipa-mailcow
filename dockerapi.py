88230125be7fcf88cb5a836287d4d0a8cea6b285
rohsiepe <falk@rohsiepe.net>
2022-11-14 22:25:53 +0100

    added module for dockerapi calls

---
+++ dockerapi.py
@@ -0,0 +1,45 @@
+import string, sys
+import requests
+import logging
+
+class DockerApiError(Exception):
+    '''The rules of the mailcow docker game were not followed'''
+
+BASE_URL = "https://dockerapi:443"
+REQUEST_TIMEOUT = 10
+
+def __get_request(url):
+    api_url = f"{BASE_URL}/{url}"
+    headers = {'Content-type': 'application/json'}
+
+    req = requests.get(api_url, verify=False, timeout=REQUEST_TIMEOUT)
+    if req.status_code != 200:
+        raise DockerApiError(f"DOCKER {url}: unexpected status {req.status_code}")
+    
+    rsp = req.json()
+    req.close()
+
+    if isinstance(rsp, list):
+        rsp = rsp[0]
+    
+    return rsp
+
+def __post_request(url, json_data):
+    api_url = f"https://dockerapi:443/{url}"
+    headers = {'Content-type': 'application/json'}
+
+    req = requests.post(api_url, verify=False, timeout=REQUEST_TIMEOUT, headers=headers, json=json_data)
+    if req.status_code != 200:
+        raise DockerApiError(f"DOCKER {url}: unexpected status {req.status_code}")
+
+    rsp = req.json()
+    req.close()
+
+    if isinstance(rsp, list):
+        rsp = rsp[0]
+
+    return rsp
+
+def test():
+    rsp = __get_request("containers/json")
+    logging.info (f"Containers info is {rsp}")
